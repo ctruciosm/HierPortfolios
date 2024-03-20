d52936b6587d8bf1c099f50c0414a7d99534c671
Carlos Trucios Maza <ctrucios@gmail.com>
2024-03-20 19:55:55 -0300

    Add files via upload
    HERC and DHRP

---
+++ R/DHRP_Portfolio.R
@@ -0,0 +1,82 @@
+#' Constrained Hierarchical Risk Parity
+#'
+#' Performs the Constrained Hierarchical Risk Parity portfolio strategy proposed  by Pfitzinger and Katzke (2019).
+#'
+#' @param covar Covariance matrix of returns. The covariance matrix will be transformed into correlation matrix and then into a distance matrix.
+#' @param graph To plot de dendrogram set this value to TRUE. By default this value is equal to FALSE.
+#' @param tau Parameter to evaluate asset similarity at the cluster edges. Default value is 1.
+#' @return portfolio weights
+#' @seealso \code{HCAA_Portfolio} \code{HRP_Portfolio} \code{HERC_Portfolio}
+#' @references Pfitzinger, J., and Katzke, N. A constrained hierarchical risk parity algorithm with cluster-based capital allocation (2019). Working Paper.
+#' @aliases DHRP_Portfolio
+#' @keywords DHRP
+#' @examples
+#' covar <- cov(mldp_returns)
+#' DHRP_Portfolio(covar)
+#' @export
+DHRP_Portfolio = function(covar, graph = FALSE, tau = 1, UB = NULL, LB = NULL) {
+
+  # https://github.com/jpfitzinger/ClusterPortfolios/blob/master/R/HRP.R
+  if (is.null(UB)) UB <- rep(1, nrow(covar))
+  if (is.null(LB)) LB <- rep(0, nrow(covar))
+  # Stage 1: Tree clustering
+  corre <- stats::cov2cor(covar)
+  distance <- sqrt(0.5 * (1 - corre))
+  # Stage 2: Divisive clustering
+  #distmat <- stats::dist(corre, method = "euclidean", diag = TRUE, upper = TRUE, p = 2) # original
+  distmat <- stats::dist(distance, method = "euclidean", diag = TRUE, upper = TRUE, p = 2)
+  #clustering <-  cluster::agnes(distmat)
+  clustering <- cluster::diana(distmat)
+  clusters_order <- clustering$order
+  clusters_height <- clustering$height
+  # Stage 3: Recursive Bisection
+  weights <- rep(1,ncol(covar))
+  index <- list(clusters_order)
+  while (length(index) > 0) {
+    new_index <- list()
+    for (i in index) {
+      aux <- floor(length(i)/2 - (length(i)/2 - 1) * tau):floor(length(i)/2 + (length(i)/2 - 1) * tau)
+      index_aux <- clusters_order %in% i
+      split_tau <- aux[which.max(clusters_height[index_aux][aux])]
+      indexa <- i[1:split_tau]
+      indexb <- i[-c(1:split_tau)]
+      covar_clustera <- as.matrix(covar[indexa, indexa])
+      covar_clusterb <- as.matrix(covar[indexb, indexb])
+      weightsa <- 1/diag(covar_clustera) / sum(1/diag(covar_clustera))
+      weightsb <- 1/diag(covar_clusterb) / sum(1/diag(covar_clusterb))
+      variance_clustera <- weightsa %*% covar_clustera %*% weightsa
+      variance_clusterb <- weightsb %*% covar_clusterb %*% weightsb
+      alpha <- as.numeric(1 - variance_clustera/(variance_clustera + variance_clusterb))
+      v_alpha <- c(alpha, 1 - alpha)
+
+      LBsub <- c(sum(LB[indexa]), sum(LB[indexb])) / c(prod(weights[indexa]), prod(weights[indexb])) # prod(weights[indexa])
+      UBsub <- c(sum(UB[indexa]), sum(UB[indexb])) / c(prod(weights[indexa]), prod(weights[indexb])) # prod(weights[indexb])
+      maxit <- 100
+      niter <- 0
+      while (any(v_alpha > UBsub | v_alpha < LBsub) && niter < maxit) {
+        alpha_tilde <-  sapply(sapply(v_alpha, min, UBsub), max, LBsub)
+        aux <- which(alpha_tilde != UBsub & alpha_tilde != LBsub)
+        alpha_tilde[aux] <- alpha_tilde[aux] + (1 - sum(alpha_tilde)) * alpha_tilde[aux] / sum(alpha_tilde[aux])
+        alpha_tilde <- alpha_tilde / sum(alpha_tilde)
+        v_alpha <- alpha_tilde
+        niter <- niter + 1
+      }
+
+      weights[indexa] <- weights[indexa] * v_alpha[1]
+      weights[indexb] <- weights[indexb] * v_alpha[2]
+      if (length(indexa) > 1) new_index <- c(new_index, list(indexa))
+      if (length(indexb) > 1) new_index <- c(new_index, list(indexb))
+      index = new_index
+    }
+
+  }
+  if (graph) plot(clustering, xlab = "", ylab = "", main = "Cluster Dendrogram - DHRP")
+  weights <- data.frame(weights)
+  row.names(weights) <- colnames(covar)
+  return(weights)
+}
+
+
+
+
+
--- R/HERC_Portfolio.R
+++ R/HERC_Portfolio.R
@@ -9,14 +9,15 @@
 #' @return portfolio weights.
 #' @seealso \code{HRP_Portfolio} and \code{HCAA_Portfolio}
 #' @references Raffinot, Thomas. "The hierarchical equal risk contribution portfolio." Available at SSRN 3237540 (2018).
-#' @references Sjostrand, Daniel, and Nima Behnejad. "Exploration of Hierarchical Clustering in Long-Only Risk-Based Portfolio Optimization." Master Thesis, Copenhagen Business School (2020).
 #' @references Tibshirani, Robert, Guenther Walther, and Trevor Hastie. "Estimating the number of clusters in a data set via the gap statistic." Journal of the Royal Statistical Society: Series B (Statistical Methodology) 63.2 (2001): 411-423.
 #' @aliases HERC_Portfolio
 #' @keywords HERC
 #' @examples
 #' covar <- cov(daily_returns)
 #' HERC_Portfolio(covar)
+#' @author Carlos Trucios and Moon Jun Kwon
 #' @export
+
 HERC_Portfolio = function(covar, linkage = "ward", graph = FALSE, clusters = NULL) {
   if (linkage %in% c("single", "complete", "average", "ward")) {
     if (linkage == "ward") {
@@ -25,11 +26,13 @@ HERC_Portfolio = function(covar, linkage = "ward", graph = FALSE, clusters = NUL
   } else {
     return("ERROR: linkage argument only supports 'single', 'complete', 'average' or 'ward' options")
   }
+  # Stage 1: Tree clustering
   corre <- stats::cov2cor(covar)
   distance <- sqrt(0.5 * (1 - corre))
   euclidean_distance <- stats::dist(distance, method = "euclidean", diag = TRUE, upper = TRUE, p = 2)
-  clustering <- fastcluster::hclust(euclidean_distance , method = linkage, members = NULL)
   n_cols <- ncol(corre)
+  # Stage 2: Quasi-Diagonalisation
+  clustering <- fastcluster::hclust(euclidean_distance , method = linkage, members = NULL)
   if (is.null(clusters)) {
     fun_clus_num <- function(x,k) list(cluster = stats::cutree(fastcluster::hclust(stats::as.dist(x) , method = linkage, members = NULL), k))
     gap <- cluster::clusGap(as.matrix(euclidean_distance), FUN = fun_clus_num, K.max = floor(n_cols/2), B = 100)
@@ -38,59 +41,61 @@ HERC_Portfolio = function(covar, linkage = "ward", graph = FALSE, clusters = NUL
   } else{
     n_clusters <- clusters
   }
-  print(sprintf("Number of clusters: %i", n_clusters))
-  elements_in_cluster <- matrix(stats::cutree(clustering, 2:n_clusters), ncol = n_clusters - 1)
-  weights_in_cluster <-  rep(1,n_cols)
-# Bisection 1
+  elements_in_cluster <- matrix(stats::cutree(clustering, 2:n_clusters), ncol = n_clusters - 1);
+  risk_in_cluster <- matrix(0, ncol = n_clusters, nrow = n_cols)
+  w <- rep(NA, n_cols)
+
+  assets_in_cluster <- elements_in_cluster[, n_clusters - 1]
+  for (i in 1:n_clusters) {
+    index_assets_in_cluster <- which(assets_in_cluster == i)
+    w[index_assets_in_cluster] <- (1/diag(covar[index_assets_in_cluster, index_assets_in_cluster])) / sum(1/diag(covar[index_assets_in_cluster, index_assets_in_cluster]))
+    risk_in_cluster[index_assets_in_cluster[1], n_clusters] <- w[index_assets_in_cluster] %*% covar[index_assets_in_cluster, index_assets_in_cluster] %*% w[index_assets_in_cluster]
+  }
+
+  for (i in (n_clusters - 1):min(2, (n_clusters - 1))) {
+    risk_in_cluster[, i] = risk_in_cluster[, i + 1]
+    for (j in 1:i) {
+      aux2_assets_in_cluster <- which(elements_in_cluster[, i - 1] == j)
+      if (length(unique(elements_in_cluster[aux2_assets_in_cluster, i])) > 1) {
+        risk_in_cluster[aux2_assets_in_cluster, i] <- 0
+        risk_in_cluster[aux2_assets_in_cluster[1], i] <- sum(risk_in_cluster[aux2_assets_in_cluster, i + 1])
+      }
+    }
+  }
+  risk_in_cluster <- matrix(risk_in_cluster[, -1], ncol = n_clusters - 1)
   indexa <- elements_in_cluster[,1] == 1
   indexb <- elements_in_cluster[,1] == 2
-  covar_clustera <- as.matrix(covar[indexa, indexa])
-  covar_clusterb <- as.matrix(covar[indexb, indexb])
-  weightsa <- 1/diag(covar_clustera) / sum(1/diag(covar_clustera))
-  weightsb <- 1/diag(covar_clusterb) / sum(1/diag(covar_clusterb))
-  # HERC does not mention how to compute the cluster variances.
-  # Since HERC is based on HRP and HCAA, we follow the HRP idea and compute the cluster variances as:
-  risk_contribution_clustera <- as.numeric(weightsa %*% covar_clustera %*% weightsa)
-  risk_contribution_clusterb <- as.numeric(weightsb %*% covar_clusterb %*% weightsb)
-  alphaa <- 1 - risk_contribution_clustera/(risk_contribution_clustera + risk_contribution_clusterb)
-  alphab <- 1 - alphaa
-  weights_in_cluster[indexa] <- alphaa*weights_in_cluster[indexa]
-  weights_in_cluster[indexb] <- alphab*weights_in_cluster[indexb]
-  # Other Bisections
+  alpha <- matrix(1, ncol = n_clusters, nrow = nrow(covar))
+  alpha[, n_clusters] <- 0
+  alpha[indexa, 1] = 1 - sum(risk_in_cluster[indexa, 1])/sum(risk_in_cluster[, 1])
+  alpha[indexb, 1] = 1 - sum(risk_in_cluster[indexb, 1])/sum(risk_in_cluster[, 1])
+
   if (n_clusters > 2) {
     for (i in 2:(n_clusters - 1)) {
-      groups <- unique(elements_in_cluster[,i - 1])
+      groups <- unique(elements_in_cluster[, i - 1])
       for (j in 1:i) {
-        index <- elements_in_cluster[, i - 1] == groups[j]
-        if (length(unique(elements_in_cluster[index, i])) > 1) {
-          sub_groups <- unique(elements_in_cluster[index, i])
-          indexa <- elements_in_cluster[ ,i] == sub_groups[1]
-          indexb <- elements_in_cluster[ ,i] == sub_groups[2]
-          covar_clustera <- as.matrix(covar[indexa, indexa])
-          covar_clusterb <- as.matrix(covar[indexb, indexb])
-          weightsa <- 1/diag(covar_clustera) / sum(1/diag(covar_clustera))
-          weightsb <- 1/diag(covar_clusterb) / sum(1/diag(covar_clusterb))
-          risk_contribution_clustera <- as.numeric(weightsa %*% covar_clustera %*% weightsa)
-          risk_contribution_clusterb <- as.numeric(weightsb %*% covar_clusterb %*% weightsb)
-          alphaa <- 1 - risk_contribution_clustera/(risk_contribution_clustera + risk_contribution_clusterb)
-          alphab <- 1 - alphaa
-          weights_in_cluster[indexa] <- alphaa*weights_in_cluster[indexa]
-          weights_in_cluster[indexb] <- alphab*weights_in_cluster[indexb]
-          break
+        if (alpha[groups[j], n_clusters] == 0) {
+          index <- elements_in_cluster[, i - 1] == groups[j]
+          if (length(unique(elements_in_cluster[index, i])) > 1) {
+            sub_groups <- unique(elements_in_cluster[index, i])
+            indexa <- elements_in_cluster[ , i] == sub_groups[1]
+            indexb <- elements_in_cluster[ , i] == sub_groups[2]
+            alpha[indexa, i] = 1 - sum(risk_in_cluster[indexa, i])/sum(risk_in_cluster[index, i])
+            alpha[indexb, i] = 1 - sum(risk_in_cluster[indexb, i])/sum(risk_in_cluster[index, i])
+          } else {
+            alpha[index, n_clusters ] <- 1
+          }
         }
       }
     }
   }
-  # Naive Risk Parity for elements in each cluster: inverse of variance
-  naive_rp_weights <- rep(1,n_cols)
-  for (j in 1:n_clusters) {
-    index <- elements_in_cluster[, n_clusters - 1] == j
-    covar_elements <- as.matrix(covar[index, index])
-    naive_rp_weights[index] <- 1/diag(covar_elements) / sum(1/diag(covar_elements))
-  }
-  weights <- weights_in_cluster * naive_rp_weights
+  alpha <- matrix(alpha[, -n_clusters], ncol = n_clusters - 1)
+  weights <- apply(alpha, 1, prod)*w
   if (graph) plot(clustering, xlab = "", ylab = "", main = "Cluster Dendrogram - HERC")
   weights <- data.frame(weights)
   row.names(weights) <- colnames(covar)
   return(weights)
 }
+
+
+
